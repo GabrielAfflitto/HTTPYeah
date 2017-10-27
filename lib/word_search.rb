e0fc179455b2b09ccaa0243f106998c548354cb6
Gabe Afflitto <gafflitto92@gmail.com>
2017-10-26 18:13:24 -0600

    WordSearch class tested and functional

--- lib/word_search.rb
+++ lib/word_search.rb
@@ -1,9 +1,22 @@
 
 class WordSearch
-  attr_reader :dictionary
-  def initialize
+  attr_reader :dictionary, :path
+  def initialize(path)
     @dictionary = File.read('/usr/share/dict/words')
+    @path = path
   end
 
+  def sample_word
+    path.split("=")[1]
+  end
+
+  def find_word
+    # word = path.split("=")[1]
+    if dictionary.include?(sample_word)
+      "#{sample_word} is a known word"
+    else
+      "#{sample_word} is not a known word"
+    end
+  end
 
 end
--- test/word_search_test.rb
+++ test/word_search_test.rb
@@ -5,16 +5,36 @@ require 'pry'
 
 class WordSeachTest < Minitest::Test
   def test_it_exists
-    ws = WordSearch.new
+    ws = WordSearch.new("/word_search?word=car")
 
     assert_instance_of WordSearch, ws
   end
 
   def test_whole_dictionary_is_populated
-    ws = WordSearch.new
+    ws = WordSearch.new("/word_search?word=car")
     length = File.read('/usr/share/dict/words').length
-    binding.pry
-    assert_equal length. ws.dictionary.length
+
+    assert_equal length, ws.dictionary.length
   end
+
+  def test_for_sample_word
+    ws = WordSearch.new("/word_search?word=car")
+
+    assert_equal "car", ws.sample_word
+  end
+
+  def test_it_can_find_a_valid_dictionary_word
+    ws = WordSearch.new("/word_search?word=car")
+
+    assert_equal "car is a known word", ws.find_word
+  end
+
+  def test_it_cannot_find_an_invalid_word
+    ws = WordSearch.new("/word_search?word=california")
+
+    assert_equal "california is not a known word", ws.find_word
+  end
+
+
 
 end
